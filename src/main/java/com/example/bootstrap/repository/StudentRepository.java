42cd6b6c259bf75a6fc0f18fe593cfd8dab00fc3
prathaprenu <prathaprenu143@gmail.com>
2023-12-01 17:18:06 +0530

    student Repoistory boolean type removed

--- src/main/java/com/example/bootstrap/repository/StudentRepository.java
+++ src/main/java/com/example/bootstrap/repository/StudentRepository.java
@@ -8,5 +8,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long>{
 
-    Boolean existsByPhoneNumber(String phoneNumber);
 }
